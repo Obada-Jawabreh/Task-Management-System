3dce33e58c51a86a9174119169d93ca5c63d48d0
Obada-Jawabreh <obadajawabreh6@gmail.com>
2024-08-09 19:55:19 +0300

    update the task

---
+++ Backend/Config/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "Tasks",
+  password: "obada4047",
+  port: 5432,
+});
+
+module.exports = pool;
